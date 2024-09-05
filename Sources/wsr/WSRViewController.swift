3eaa8cde15783e92e6033e35377bc4d4deec8258
William Rena <wilyamx@gmail.com>
2024-09-05 14:12:53 +0800

    [Packager] Default navigation style

--- Sources/wsr/WSRViewController.swift
+++ Sources/wsr/WSRViewController.swift
@@ -29,4 +29,31 @@ open class WSRViewController: UIViewController {
     open func setupConstraints() {}
     open func setupBindings() {}
     open func setupActions() {}
+    
+    // MARK: - Navigation
+    
+    public func wsrNavigationBarDefaultStyle(backgroundColor: UIColor = .black, tintColor: UIColor = .white) {
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithTransparentBackground()
+            appearance.backgroundColor = backgroundColor
+            appearance.shadowColor = .clear
+
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().compactAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
+            let titleAttribute = [
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: .regular),
+                NSAttributedString.Key.foregroundColor: UIColor.white
+            ]
+            appearance.titleTextAttributes = titleAttribute
+        } else {
+            let navigationBar = navigationController?.navigationBar
+            navigationBar?.standardAppearance.backgroundColor = backgroundColor
+            navigationBar?.standardAppearance.shadowColor = .clear
+        }
+        navigationController?.navigationBar.tintColor = tintColor
+        navigationItem.backButtonTitle = ""
+    }
 }
