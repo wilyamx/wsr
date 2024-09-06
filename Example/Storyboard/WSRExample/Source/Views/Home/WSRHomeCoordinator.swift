5c2250c998fa528fd0a19ea3c5e77b6e05bd0486
William Rena <wilyamx@gmail.com>
2024-09-06 10:52:38 +0800

    [Example] Button types

--- Example/Storyboard/WSRExample/Source/Views/Home/WSRHomeCoordinator.swift
+++ Example/Storyboard/WSRExample/Source/Views/Home/WSRHomeCoordinator.swift
@@ -5,8 +5,33 @@
 //  Created by William S. Rena on 9/5/24.
 //
 
-import Foundation
+import UIKit
+import wsr
 
-class WSRHomeCoordinator {
-    
+class WSRHomeCoordinator: WSRCoordinatorProtocol {
+    var childCoordinators = [WSRCoordinatorProtocol]()
+    var navigationController: UINavigationController?
+
+    var window: UIWindow?
+
+    init(window: UIWindow?, navigationController: UINavigationController?) {
+        self.window = window
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let viewController = WSRHomeViewController()
+        viewController.coordinator = self
+
+        navigationController?.pushViewController(viewController, animated: false)
+
+        guard let navigationController = navigationController
+        else {
+            window?.rootViewController = viewController
+            return
+        }
+
+        viewController.wsr_NavigationBarDefaultStyle(backgroundColor: UIColor.accent, tintColor: .white)
+        window?.rootViewController = navigationController
+    }
 }
--- Example/Storyboard/WSRExample/Source/Views/Home/WSRHomeViewController.swift
+++ Example/Storyboard/WSRExample/Source/Views/Home/WSRHomeViewController.swift
@@ -7,12 +7,87 @@
 
 import UIKit
 import wsr
+import SuperEasyLayout
 
-class WSRHomeViewController {
+class WSRHomeViewController: WSRViewController {
+    weak var coordinator: WSRHomeCoordinator?
+    
     private lazy var verticalStackView: UIStackView = {
         let view = UIStackView()
         view.axis = .vertical
-        view.spacing = 6
+        view.spacing = 10
         return view
     }()
+    
+    private lazy var activeButton: WSRButton = {
+        let view = WSRButton()
+        view.text = "ACTIVE"
+        view.colorStyle = .active
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private lazy var inactiveButton: WSRButton = {
+        let view = WSRButton()
+        view.text = "INACTIVE"
+        view.colorStyle = .inactive
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    private lazy var disabledButton: WSRButton = {
+        let view = WSRButton()
+        view.text = "DISABLED"
+        view.colorStyle = .inactive
+        view.isEnabled = false
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Setups
+    
+    override func setupNavigation() {
+        title = "WSR Swift Packager"
+        wsr_NavigationBarDefaultStyle()
+    }
+    
+    override func setupLayout() {
+        view.backgroundColor = .white
+        
+        addSubviews([
+            verticalStackView.addArrangedSubviews([
+                activeButton,
+                inactiveButton,
+                disabledButton
+            ])
+        ])
+    }
+    
+    override func setupConstraints() {
+        verticalStackView.left == view.left + 20
+        verticalStackView.right == view.right - 20
+        verticalStackView.centerY == view.centerY
+        
+        activeButton.height == 44
+        inactiveButton.height == 44
+        disabledButton.height == 44
+    }
+    
+    override func setupActions() {
+        activeButton.tapHandlerAsync = { _ in
+            wsrLogger.info(message: "1111")
+        }
+        inactiveButton.tapHandlerAsync = { _ in
+            wsrLogger.info(message: "2222")
+        }
+        disabledButton.tapHandlerAsync = { _ in
+            wsrLogger.info(message: "3333")
+        }
+    }
 }
