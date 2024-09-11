6723a608c12af8b2635b5196ae93c7f1d41dbd5e
William Rena <wilyamx@gmail.com>
2024-09-11 10:35:54 +0800

    [Example] Alert Async

--- Example/Storyboard/WSRExample/Source/Views/PresentViewController/WSREPresentViewController.swift
+++ Example/Storyboard/WSRExample/Source/Views/PresentViewController/WSREPresentViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import WSRComponents
 import WSRUtils
+import SuperEasyLayout
 
 class WSREPresentViewController: WSREViewController {
+    
+    private lazy var verticalStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 10
+        return view
+    }()
+    
+    private lazy var alertButton: WSRButton = {
+        let view = WSRButton()
+        view.text = "ALERT"
+        view.colorStyle = .active
+        view.layer.cornerRadius = 8
+        return view
+    }()
     
     private var navigationBar: WSRENavigationBar? {
         navigationController?.navigationBar as? WSRENavigationBar
@@ -24,6 +41,12 @@ class WSREPresentViewController: WSREViewController {
     override func setupLayout() {
         super.setupLayout()
         
+        addSubviews([
+            verticalStackView.addArrangedSubviews([
+                alertButton
+            ])
+        ])
+        
         let randomInt = Int.random(in: 0..<2)
         if randomInt == 0 {
             navigationBar?.showProfileButtons = true
@@ -33,13 +56,53 @@ class WSREPresentViewController: WSREViewController {
         }
     }
     
+    override func setupConstraints() {
+        verticalStackView.left == view.left + 20
+        verticalStackView.right == view.right - 20
+        verticalStackView.centerY == view.centerY
+        
+        alertButton.height == 44
+    }
+    
     override func setupActions() {
         navigationBar?.profileTapHandlerAsync = { _ in
             wsrLogger.info(message: "navigationBar?.profileTapHandlerAsync")
         }
-        
         navigationBar?.closeTapHandlerAsync = { [weak self] _ in
             self?.dismiss(animated: true)
         }
+        
+        alertButton.tapHandlerAsync = { [weak self] _ in
+            guard let self else { return }
+            
+            let isDelete = await showChatRoomDeleteAlert(in: self, chatName: "Lorem Ipsum")
+            let password = await showChatRoomPasswordAlert(in: self, chatName: "Lorem Ipsum")
+            debugPrint("Alert selection: isDelete? \(isDelete), password? \(password ?? "??")")
+        }
+    }
+    
+    // MARK: - Alert
+    
+    @MainActor
+    @discardableResult
+    private func showChatRoomDeleteAlert(in viewController: UIViewController, chatName: String) async -> Bool {
+        return await WSRAsyncAlertController<Bool>(
+            message: "Are you sure you want to delete the chat room \(chatName) and all it's messages?",
+            title: "Delete confirmation"
+        )
+        .addButton(title: "Yes, I want to delete this chat room", style: .destructive, returnValue: true)
+        .addButton(title: "Cancel", returnValue: false)
+        .register(in: viewController)
+    }
+    
+    @MainActor
+    @discardableResult
+    private func showChatRoomPasswordAlert(in viewController: UIViewController, chatName: String) async -> String? {
+        return await AsyncInputAlertController<String>(
+            title: "\(chatName) requires a password",
+            message: "Please enter the password."
+        )
+        .addButton(title: "Ok")
+        .register(in: viewController)
     }
 }
