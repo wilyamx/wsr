4878313ab2246d7865964669ff5d6d401e1e85e0
William Rena <wilyamx@gmail.com>
2024-09-12 08:12:11 +0800

    [Packager] Delay

---
+++ Sources/WSRComponents/Extensions/Task+WSR.swift
@@ -0,0 +1,14 @@
+//
+//  Task+WSR.swift
+//
+//
+//  Created by William S. Rena on 9/12/24.
+//
+
+import Foundation
+
+public extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: TimeInterval) async {
+        try? await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
+    }
+}
