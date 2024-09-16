8bcd7393ddee33e45bb792fce63be564b4ee9324
William Rena <wilyamx@gmail.com>
2024-09-16 19:05:17 +0800

    [Packager] New

--- Sources/WSRComponents/Extensions/String+WSR.swift
+++ Sources/WSRComponents/Extensions/String+WSR.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 public extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+    
     func getAttributedString(with font: UIFont,
                              color: UIColor? = .darkText,
                              attributes: [NSAttributedString.Key: Any]? = nil) -> NSMutableAttributedString {
---
+++ Sources/WSRMedia/WSRImageCache.swift
@@ -0,0 +1,31 @@
+//
+//  WSRImageCache.swift
+//
+//
+//  Created by William S. Rena on 9/16/24.
+//
+
+import UIKit
+
+/**
+    https://xavier7t.com/image-caching-in-swiftui
+ */
+public class WSRImageCache {
+    public static let shared = WSRImageCache()
+
+    private let cache = NSCache<NSString, UIImage>()
+
+    private init() {}
+
+    public func set(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+    }
+
+    public func get(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+    
+    public func remove(forKey key: String) {
+        return cache.removeObject(forKey: key as NSString)
+    }
+}
